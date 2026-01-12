c8f9acee1da2d5244458fce2a712d3480f479f0c
vishi <tvishesh44@gmail.com>
2026-01-12 10:06:05 -0700

    Initialize LED pin

--- app/src/main.c
+++ app/src/main.c
@@ -2,10 +2,32 @@
  * main.c
  */
 
+modules/
+zephyr/
+
 #include <zephyr/kernel.h>
 #include <zephyr/drivers/gpio.h>
 
+#define LED0_NODE DT_ALIAS(led0)
+
+static const struct gpio_dt_spec led0 = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
+{
+    /* data */
+};
+
+
 int main(void) {
+    int ret;
+
+    if(!gpio_is_ready_dt(&led0)){
+        return -1;
+    }
+
+    ret = gpio_pin_configure_dt(&led0, GPIO_OUTPUT_ACTIVE);
+    if (ret < 0) {
+        return ret;
+    }
+    
     while (1) {
 
     }
