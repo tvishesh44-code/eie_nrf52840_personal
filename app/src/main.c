f6f14914476d9b0a78de15b9a2ae47dc7450f4d6
vishi <tvishesh44@gmail.com>
2026-01-13 04:12:07 -0700

    2nd LED

--- app/src/main.c
+++ app/src/main.c
@@ -6,13 +6,20 @@
 #include <zephyr/drivers/gpio.h>
 
 #define LED0_NODE DT_ALIAS(led0)
+#define LED1_NODE DT_ALIAS(led1)
 
 static const struct gpio_dt_spec led0 = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
+static const struct gpio_dt_spec led1 = GPIO_DT_SPEC_GET(LED1_NODE, gpios);
+
 
 int main(void) {
-    int ret;
+    int ret, ret1;
 
     if(!gpio_is_ready_dt(&led0)){
+        return -1;
+    }
+
+    if(!gpio_is_ready_dt(&led1)){
         return -1;
     }
 
@@ -21,8 +28,17 @@ int main(void) {
         return ret;
     }
 
+    ret1 = gpio_pin_configure_dt(&led1, GPIO_OUTPUT_ACTIVE);
+    if (ret1 < 0) {
+        return ret1;
+    }
+
     while (1) {
         gpio_pin_toggle_dt(&led0);
+
+        k_msleep(1000);
+
+        gpio_pin_toggle_dt(&led1);
 
         k_msleep(1000);
     }
