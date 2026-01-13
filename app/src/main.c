925feae0827637b4d28ddf53adc3c49ceda27686
vishi <tvishesh44@gmail.com>
2026-01-13 04:20:44 -0700

    All 4 LED

--- app/src/main.c
+++ app/src/main.c
@@ -7,19 +7,31 @@
 
 #define LED0_NODE DT_ALIAS(led0)
 #define LED1_NODE DT_ALIAS(led1)
+#define LED2_NODE DT_ALIAS(led2)
+#define LED3_NODE DT_ALIAS(led3)
 
 static const struct gpio_dt_spec led0 = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
 static const struct gpio_dt_spec led1 = GPIO_DT_SPEC_GET(LED1_NODE, gpios);
+static const struct gpio_dt_spec led2 = GPIO_DT_SPEC_GET(LED2_NODE, gpios);
+static const struct gpio_dt_spec led3 = GPIO_DT_SPEC_GET(LED3_NODE, gpios);
 
 
 int main(void) {
-    int ret, ret1;
+    int ret, ret1, ret2, ret3;
 
     if(!gpio_is_ready_dt(&led0)){
         return -1;
     }
 
     if(!gpio_is_ready_dt(&led1)){
+        return -1;
+    }
+
+    if(!gpio_is_ready_dt(&led2)){
+        return -1;
+    }
+
+    if(!gpio_is_ready_dt(&led3)){
         return -1;
     }
 
@@ -33,13 +45,21 @@ int main(void) {
         return ret1;
     }
 
+    ret2 = gpio_pin_configure_dt(&led2, GPIO_OUTPUT_ACTIVE);
+    if (ret1 < 0) {
+        return ret2;
+    }
+
+    ret3 = gpio_pin_configure_dt(&led3, GPIO_OUTPUT_ACTIVE);
+    if (ret1 < 0) {
+        return ret3;
+    }
+
     while (1) {
         gpio_pin_toggle_dt(&led0);
-
-        k_msleep(1000);
-
         gpio_pin_toggle_dt(&led1);
-
+        gpio_pin_toggle_dt(&led2);
+        gpio_pin_toggle_dt(&led3);
         k_msleep(1000);
     }
 
