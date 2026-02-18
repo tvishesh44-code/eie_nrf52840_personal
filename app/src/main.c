a07e698d7cf0c14b1076ef8a2221e768f094c265
vishi <tvishesh44@gmail.com>
2026-02-17 22:51:11 -0700

    Added Hello world to screen

--- app/src/main.c
+++ app/src/main.c
@@ -1,27 +1,35 @@
-/**
- * @file main.c
- */
-
 #include <inttypes.h>
 
 #include <zephyr/kernel.h>
 #include <zephyr/sys/printk.h>
+#include <zephyr/device.h>
+#include <zephyr/drivers/display.h>
+
+#include "lvgl.h"
 
 #include "BTN.h"
-#include "LED.h"
 
 #define SLEEP_MS 1
 
-int main(void) {
-  if (0 > BTN_init()) {
-    return 0;
-  }
-  if (0 > LED_init()) {
-    return 0;
-  }
+static const struct device *display_dev = DEVICE_DT_GET(DT_CHOSEN(zephyr_display));
+static lv_obj_t *screen = NULL;
 
-  while (1) {
-    k_msleep(SLEEP_MS);
-  }
-  return 0;
+int main(void)
+{
+    if(!device_is_ready(display_dev)){
+        return 0;
+    }
+    screen = lv_screen_active();
+    if(screen == NULL){
+        return 0;
+    }
+
+    lv_obj_t *label = lv_label_create(screen);
+    lv_label_set_text(label, "Hello World!");
+    display_blanking_off(display_dev);
+
+    while(1) {
+        lv_timer_handler();
+        k_msleep(SLEEP_MS);
+    }
 }
